92466c6de15ef02f65b8028dee07ac1da27a4c64
yaolee <144488087+yaoolee@users.noreply.github.com>
2025-06-02 20:52:58 -0400

    Create project and configure it

---
+++ playlist-pilot/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace playlist_pilot.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
